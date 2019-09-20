99395cb1972a7b3afb9bd614878edd4dacd04464
Youssef Idelhadj <youssef.idelhadj@live.fr>
2019-09-20 23:51:23 +0200

    Gestion de pagination avec les services

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -14,12 +14,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminAdController extends AbstractController
 {
     /**
-     * @Route("/admin/ads", name="admin_ads_index")
+     * @Route("/admin/ads/{page<\d+>?1}", name="admin_ads_index")
      */
-    public function index(AdRepository $repo)
+    public function index(AdRepository $repo, $page, PaginationService $pagination)
     {
+        $pagination->setEntityClass(Ad::class)
+                   ->setPage($page)
+        ;
+   
         return $this->render('admin/ad/index.html.twig', [
-            'ads' => $repo->findAll()
+            'pagination' => $pagination
         ]);
     }
 
--- src/Controller/AdminBookingController.php
+++ src/Controller/AdminBookingController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Booking;
 use App\Form\BookingType;
 use App\Form\AdminBookingType;
+use App\Service\PaginationService;
 use App\Repository\BookingRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -15,14 +16,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminBookingController extends AbstractController
 {
     /**
-     * @Route("/admin/bookings", name="admin_booking_index")
+     * @Route("/admin/bookings/{page<\d+>?1}", name="admin_booking_index")
      */
-    public function index(BookingRepository $repo)
+    public function index(BookingRepository $repo,$page, PaginationService $pagination)
     {
-        $bookings = $repo->findAll();
-
+        $pagination->setEntityClass(Booking::class)
+                   ->setPage($page)
+        ;
+   
         return $this->render('admin/booking/index.html.twig', [
-            'bookings' => $bookings
+            'pagination' => $pagination
         ]);
     }
 
